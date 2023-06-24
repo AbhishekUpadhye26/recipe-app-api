ffd2b7fbb96fadd04bb58046abf0c290877ec2ab
AbhishekUpadhye26 <abhi.upadhye2611@gmail.com>
2023-06-24 08:41:34 +0530

    linting fixed in tests.py and calc.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class ClacTests(SimpleTestCase):
     """
     Test the calc module.
@@ -17,7 +18,6 @@ class ClacTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting numbers.""" 
